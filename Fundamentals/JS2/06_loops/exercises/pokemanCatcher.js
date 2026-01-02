2024dae34938f89f8ccb68b51cc4be8ecf55e367
fahr-designs <201617505+fahr-designs@users.noreply.github.com>
2026-01-02 14:07:07 -0600

    loop exercises refreshers.

---
+++ Fundamentals/JS2/06_loops/exercises/pokemanCatcher.js
@@ -0,0 +1,15 @@
+/**
+ * Write a for...of loop that iterates through our pokemonList array.
+ * 
+ * Inside the block of the for...of loop, use console.log() and string interpolation as 
+ * modeled above to log each element in the pokemon array within the string 'You caught a X!' 
+ * 
+ * For example, the first iteration of the loop should print You caught a Pikachu! to the console.
+ * 
+ * One of the elements, 'Yoshi', is not a Pokemon. 
+ * If you encounter 'Yoshi', use continue to skip this element before it is logged to the console.
+ */
+
+const pokemonList = ['Ditto', 'Charizard', 'Squirtle', 'Yoshi', 'Gengar'];
+
+// Write your code below
---
+++ Fundamentals/JS2/06_loops/exercises/spelling.js
@@ -0,0 +1,9 @@
+/**
+ * Write a program to help us improve our spelling skills.
+ * 
+ * Given the spellingWord, use a for...of loop to log each letter using console.log().
+ */
+
+const spellingWord = 'osawatemie';
+
+// Write your code below
